28fd588045e331c5e6a8cca178b3f7caa8515c6f
jrmaiafs <67764559+jrmaiafs@users.noreply.github.com>
2021-05-07 23:52:37 -0300

    add edit comment

---
+++ endpoints/comment_modify.php
@@ -0,0 +1,45 @@
+<?php 
+
+    function api_comment_delete($request) {
+        $post_id = $request['id'];
+
+        $comments = get_comments([
+            'post_id' => $post_id
+        ]);
+
+        return rest_ensure_response($comments);
+    }
+ 
+
+    function register_api_comment_delete() {
+        register_rest_route( 'api', '/comment/delete(?P<id>[0-9]+)', [
+            'methods' => WP_REST_Server::DELETABLE,
+            'callback' => 'api_comment_delete',
+        ]);
+    };
+
+    add_action( 'rest_api_init', 'register_api_comment_delete');
+
+    // comment edit
+
+    function api_comment_edit($request) {
+        $post_id = $request['id'];
+
+        $comments = get_comments([
+            'post_id' => $post_id
+        ]);
+
+        return rest_ensure_response($comments);
+    }
+ 
+
+    function register_api_comment_edit() {
+        register_rest_route( 'api', '/comment/edit(?P<id>[0-9]+)', [
+            'methods' => WP_REST_Server::CREATABLE,
+            'callback' => 'api_comment_edit',
+        ]);
+    };
+
+    add_action( 'rest_api_init', 'register_api_comment_edit');
+
+?>
